8802d3428d067849f1ffc6059e7fc16c9918a96d
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-08-18 13:56:13 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ae8a21e3d01449f00df7846a226f7819'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cb212c12-2a95-468c-a62b-537fa4e5c06d'
+};
